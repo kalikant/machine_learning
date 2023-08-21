97cb00daa2878836ad138d2920e918122497ea33
Kalikant <kalikant@users.noreply.github.com>
2023-08-22 01:09:06 +0800

    Update lstm_example_1.py
    early stopping

--- lstm_example_1.py
+++ lstm_example_1.py
@@ -107,6 +107,10 @@ class Trainer:
             f1 = f1_score(all_labels, all_preds)
             print(f"Validation Accuracy: {accuracy:.4f}, F1 Score: {f1:.4f}")
 
+            avg_loss = total_loss / len(self.val_loader)
+            print(f"Validation Loss: {avg_loss:.4f}")
+            return avg_loss
+
 # Step 5: Evaluation class
 class Evaluator:
     def __init__(self, model, test_loader, device):
@@ -133,9 +137,6 @@ class Evaluator:
             f1 = f1_score(all_labels, all_preds)
             print(f"Test Accuracy: {accuracy:.4f}, F1 Score: {f1:.4f}")
 
-            avg_loss = total_loss / len(self.val_loader)
-            print(f"Validation Loss: {avg_loss:.4f}")
-            return avg_loss
 
 # Step 6: Prediction class
 class Predictor:
